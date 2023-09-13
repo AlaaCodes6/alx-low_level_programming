a7347fdf61813cb0489aef464f6af4dcf2354193
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-12 19:06:56 -0700

    table100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,9 @@
 
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int prod, mult, num;
 
-	for (num <= 15 && n >= 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (num = 0; num <= n; ++num)
 		{
@@ -24,12 +24,12 @@ void print_times_table(int n)
 				_putchar(' ');
 			if (prod <= 99)
 				_putchar(' ');
+
 			if (prod >= 100)
 			{
 				_putchar((prod / 100) + 48);
 				_putchar((prod / 10) % 10 + 48);
-			}
-			else if (prod <= 99 && prod >= 10)
+			} else if (prod <= 99 && prod >= 10)
 				_putchar((prod / 10) + 48);
 			_putchar(prod % 10) + 48);
 		}
