bdbc89c81b32332a9f4068251700868530682179
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-14 12:49:21 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * _isupper - checks for uppercase characters	
+ *
+ * @c: input for alphabets
+ *
+ * Return: 1 if uppercase otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
