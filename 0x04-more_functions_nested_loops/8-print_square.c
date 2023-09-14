a60bfb740cc48d0e9f5da25111dc592469dba99e
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-14 15:23:40 -0700

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using #
+ *
+ * @size: size of the square
+ *
+ * Return: always 0 (success)
+ */
+
+void print_square(int size)
+{
+	int row, colomn;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (column = 1; column <= size; column++)
+			_putchar('#');
+		_putchar('\n');
+	}
+
+
