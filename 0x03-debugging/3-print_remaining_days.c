e5cf8cfe5200b925e089a05c0139c454cc99c56c
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-14 03:50:43 -0700

    again

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,6 +12,11 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	/**
+	* It is considered leap year when it
+	* can be devided by 100 and 400 or by
+	* 4 evenly
+	*/
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
