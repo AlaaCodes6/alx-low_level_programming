bc7a4c1e72d98486e57e7d0e00c38e5543374a67
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-18 14:53:22 -0700

    swap

---
+++ 1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
