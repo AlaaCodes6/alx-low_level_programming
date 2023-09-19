6fba4f603c86117e15a37af6e13ec05e514acdfb
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-18 19:54:23 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,12 @@
 void puts_half(char *str)
 {
 	int i;
-	int n;
-	int count = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-		count++;
-		n = (count - 1) / 2;
-	for (i = n + 1; str[i] != '\0'; i++)
+		i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
 		_putchar(str[i]);
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
