db429ef306646c8050a7ed4f93d8b94a64c5f05b
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-18 17:58:17 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,23 @@
-dsdid
+#include "main.h"
+
+/**
+ * print_rev -  prints reversed string, followed by a new line
+ *
+ * @s: input parameter
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
