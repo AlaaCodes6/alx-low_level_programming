e95cfe2965d655a8eef518e0d09d30c2b519da05
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-18 17:37:54 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
-ewewe
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout
+ *
+ * @str: input parameter
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
