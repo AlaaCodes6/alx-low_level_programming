dfbfe6837c9087ca8d265664dcf543f670a3365c
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-21 14:08:39 -0700

    buffer2

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,70 +1,141 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * isPrintableASCII - function that determines if n is a printable ASCII char
- *
- * @n: input
- *
- * Return: 1 if tre, 0 if false
+   * rev_string - reverse array
+   * @n: integer params
+   * Return: 0
  */
 
-int isPrintableASCII(int n)
-{
-	return (n >= 32 && n <= 126);
-}
-
-
-/**
- * printHexes - function that prints hex values
- *
- * @b: input
- * @start: starting point
- * @end: finishing point
- *
- * Return: nothing
- */
-
-void printHexes(char *b, int start, int end)
+void rev_string(char *n)
 {
 	int i = 0;
+	int j = 0;
+	char temp;
 
-	while (i < 10)
+	while (*(n + i) != '\0')
 	{
-		if (i < end)
-			printf("%02x", *(b + start + i));
-		else
-			printf("  ");
-		if (i % 2)
-			printf(" ");
-		i++;
+	i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+	temp = *(n + j);
+	*(n + j) = *(n + i);
+	*(n + i) = temp;
 	}
 }
 
 /**
- * print_buffer - function that prints a buffer
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+	i++;
+	while (*(n2 + j) != '\0')
+	j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+	return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+	if (i < 0)
+	val1 = 0;
+	else
+	val1 = *(n1 + i) - '0';
+	if (j < 0)
+
+	val2 = 0;
+	else
+	val2 = *(n2 + j) - '0';
+	temp_tot = val1 + val2 + overflow;
+	if (temp_tot >= 10)
+	overflow = 1;
+	else
+	overflow = 0;
+	if (digits >= (size_r - 1))
+	return (0);
+	*(r + digits) = (temp_tot % 10) + '0';
+	digits++;
+	j--;
+	i--;
+	}
+	if (digits == size_r)
+	return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+}
+
+
+
+
+Task 104
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - main function
  *
- * @b: input
- * @size: size of a bufferr
+ * @b: The buffer.
  *
- * Return: nothing
+ * @size: size of buffer
+ *
+ * Return: 0
  */
 
 void print_buffer(char *b, int size)
 {
-	int start, end;
+	int x;
+	int y;
+	int z;
 
-	if (size > 0)
+	x = 0;
+
+	if (size <= 0)
 	{
-		for (start = 0; start < size; start += 10)
-		{
-			end = (size - start < 10) ? size - start : 10;
-			printf("%08x: ", start);
-			printHexes(b, start, end);
-			printASCII(b, start, end);
-			printf("\n");
-		}
+	printf("\n");
+	return;
 	}
+	while (x < size)
+	{
+	y = size - x < 10 ? size - x : 10;
+	printf("%08x: ", x);
+	for (z = 0; z < 10; z++)
+	{
+	if (z < y)
+	printf("%02x", *(b + x + z));
 	else
-		printf("\n")
+	printf("  ");
+	if (z % 2)
+	{
+	printf(" ");
+	}
+	}
+	for (z = 0; z < y; z++)
+	{
+	int c = *(b + x + z);
+
+	if (c < 32 || c > 132)
+	{
+	c = '.';
+	}
+	printf("%c", c);
+	}
+	printf("\n");
+	x += 10;
+	}
 }
+
