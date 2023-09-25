004985d20b0889b904214d29f0317530ce0d94ee
AlaaCodes6 <alaacodes6@gmail.com>
2023-09-25 03:23:37 -0700

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ void print_diagsums(int *a, int size)
 		sum1 = sum1 + a[i * size + i];
 	}
 
-	for (i = size - 1; y >= 0; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
 		sum2 += a[i * size + (size - i - 1)];
 	}
