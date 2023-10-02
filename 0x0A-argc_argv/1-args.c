9591ea4b25f6c677f7741a3cfed8370db6f84b89
AlaaCodes6 <alaacodes6@gmail.com>
2023-10-02 11:46:06 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the number of arguments passed into it.
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
