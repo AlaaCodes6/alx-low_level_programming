4a54b90ddc7af172c3e73c169ad0c17020600ceb
AlaaCodes6 <alaacodes6@gmail.com>
2023-10-11 13:51:38 -0700

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,77 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - Returns the sum of 2 integers
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: The sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference of 2 integers
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: The difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of 2 integers
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: The product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the division of 2 integers
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: The division of a by b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of the division of two integers.
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: The remainder of the division of a by b.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
