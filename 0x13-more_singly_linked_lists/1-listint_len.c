6d277d6df46f43ef433347b19e4936964b101544
AlaaCodes6 <alaacodes6@gmail.com>
2023-10-24 14:33:59 -0700

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in
+ * linked list
+ *
+ * @h: head
+ *
+ * Return: numbers of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
