b6efcf77908088928c618108581415d2eae91081
AlaaCodes6 <alaacodes6@gmail.com>
2023-10-30 14:08:15 -0700

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - checks if there's error
+ *
+ * @file_from: input
+ * @file_to: input
+ * @argv: arguments vector
+ *
+ * Return: nothing
+ */
+
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, error_close;
+	ssize_t nchars, nprint;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nprint = write(file_to, buf, nchars);
+		if (nprint == -1)
+			error_file(0, -1, argv);
+	}
+
+	error_close = close(file_from);
+	if (error_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	error_close = close(file_to);
+	if (error_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
